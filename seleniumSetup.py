b1c4605ece1d072b941539a8d673c3b0a616df9d
Matt Martin <57736727+robotmatt@users.noreply.github.com>
2025-11-10 16:30:13 -0600

    added a check in the Selenium Setup if machine is Windows or Mac

--- seleniumSetup.py
+++ seleniumSetup.py
@@ -1,15 +1,23 @@
+import os
+
 from selenium import webdriver
 
+if os.name == 'nt': # check if on windows 'nt'
+    def seleniumSetup():
+        service = webdriver.ChromeService()
+        browser = webdriver.Chrome(service=service)
+        return browser
 
-def seleniumSetup():
-#    browser = webdriver.Chrome('/Users/chromedriver') #might have to point to a different location
-    #service = webdriver.ChromeService(executable_path= '/Users/chromedriver')
-    service = webdriver.ChromeService(executable_path= '/opt/homebrew/bin/chromedriver')
-    browser = webdriver.Chrome(service=service)
+if os.name == 'posix': # check if on mac 'postix'
+    def seleniumSetup():
+        #might have to point to a different location depending on where your chromedriver is installed
+        service = webdriver.ChromeService(executable_path= '/opt/homebrew/bin/chromedriver')
+        browser = webdriver.Chrome(service=service)
+        return browser
 
 # the following two lines are used for Safari
 #    browser = webdriver.Safari()
 #    browser.maximize_window()
-    return browser
+
 
 
