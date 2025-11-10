c1cac156c664f8e9dab76c7919faee59ff5aaa37
Matt Martin <57736727+robotmatt@users.noreply.github.com>
2025-11-10 11:16:52 -0600

    Fixing cromedriver for homebrew path

--- seleniumSetup.py
+++ seleniumSetup.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 
+
 def seleniumSetup():
-    browser = webdriver.Chrome('/usr/local/bin/chromedriver') #might have to point to a different location
+#    browser = webdriver.Chrome('/Users/chromedriver') #might have to point to a different location
+    #service = webdriver.ChromeService(executable_path= '/Users/chromedriver')
+    service = webdriver.ChromeService(executable_path= '/opt/homebrew/bin/chromedriver')
+    browser = webdriver.Chrome(service=service)
 
 # the following two lines are used for Safari
 #    browser = webdriver.Safari()
 #    browser.maximize_window()
     return browser
+
+
