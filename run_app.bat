dcbe99812cbb2c10c6935319c08a31cd67564bb2
Matt Martin <57736727+robotmatt@users.noreply.github.com>
2025-11-23 18:55:23 -0600

    updated run_app.bat to install binarys only and not build from scratch

--- run_app.bat
+++ run_app.bat
@@ -1,6 +1,9 @@
 @echo off
+echo Upgrading pip...
+pip install --upgrade pip
+
 echo Installing dependencies...
-pip install -r requirements.txt
+pip install streamlit selenium webdriver-manager --only-binary=:all:
 
 echo Starting AutoNB...
 python -m streamlit run app.py
